437c52ff36aae54a33fe4d4713c0c5a13d9741e9
Sofia Bargues <barguessofia@gmail.com>
2024-02-01 20:50:09 +0000

    funciona

--- src/components/CssGrid.tsx
+++ src/components/CssGrid.tsx
@@ -3,12 +3,14 @@ import { data } from "../data";
 function Card({
   item,
   i,
+  shape,
 }: {
   item: {
     title: string;
     value: number;
   };
   i: number;
+  shape: "horizontal" | "vertical" | "square";
 }) {
   const boxStyle =
     "bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center";
@@ -17,8 +19,12 @@ function Card({
     <div
       key={i}
       className={`${boxStyle} ${
-        i === 0 || i === 4 || i === 5 || i === 6 ? "md:col-span-2" : ""
-      }${i === 2 ? "md:row-span-2" : ""}`}
+        shape === "horizontal"
+          ? "md:col-span-2"
+          : shape === "vertical"
+          ? "md:row-span-2"
+          : ""
+      }`}
     >
       <h2 className="text-xl text-gray-600">{item.title}</h2>
       <p className="font-bold text-2xl">{item.value}</p>
@@ -30,7 +36,18 @@ const CssGrid = () => {
   return (
     <div className="grid md:grid-cols-4 auto-rows-[300px] gap-4 my-10">
       {data.map((item, i) => (
-        <Card key={i} item={item} i={i} />
+        <Card
+          key={i}
+          item={item}
+          i={i}
+          shape={
+            i === 0 || i === 4 || i === 5 || i === 6
+              ? "horizontal"
+              : i === 2
+              ? "vertical"
+              : "square"
+          }
+        />
       ))}
     </div>
   );
